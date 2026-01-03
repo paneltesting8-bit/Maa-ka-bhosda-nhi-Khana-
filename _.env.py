bc38477edcd2e596498ef3b622d162822e2a845f
paneltesting8-bit <paneltesting8@gmail.com>
2026-01-04 01:34:57 +0530

    Add files via upload

---
+++ _.env.py
@@ -0,0 +1,2 @@
+TELEGRAM_BOT_TOKEN=8363486220:AAF7zDmmz-BQutPirhwpcjErlWNhEuzrZJI
+MONGODB_URI=mongodb+srv://ElectraOp:BGMI272@cluster0.1jmwb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
